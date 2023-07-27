e4b6c288d4697dc45173e4911d29e102b0c431d0
Perigrine333 <86404559+Perigrine333@users.noreply.github.com>
2023-07-27 00:02:43 -0400

    Initial commit

---
+++ src/main/java/net/perigrine3/minerpunk/MinerpunkModClient.java
@@ -0,0 +1,10 @@
+package net.perigrine3.minerpunk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinerpunkModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
